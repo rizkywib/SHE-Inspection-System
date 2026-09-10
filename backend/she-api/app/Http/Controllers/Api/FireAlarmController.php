3343157e5107f4461e5a427b039b7633ee3a0e6f
pteodev <it.application@ecogreenoleo.com>
2026-09-10 11:02:28 +0700

    Perbaiki validasi exists fire_alarm_locations -> fire_alarm_location pada FireAlarmController

--- backend/she-api/app/Http/Controllers/Api/FireAlarmController.php
+++ backend/she-api/app/Http/Controllers/Api/FireAlarmController.php
@@ -25,7 +25,7 @@ class FireAlarmController extends Controller
         $validator = Validator::make($request->all(), [
             'reference_no' => 'nullable|string|max:40|unique:fire_alarm_inspections,reference_no',
             'inspection_date' => 'required|date',
-            'location_id' => 'nullable|exists:fire_alarm_locations,id_location',
+            'location_id' => 'nullable|exists:fire_alarm_location,id_location',
             'area_id' => 'nullable|exists:areas,id',
             'qr_code_id' => 'nullable|exists:asset_qr_codes,id',
             'inspector_id' => 'nullable|exists:users,id',
@@ -88,7 +88,7 @@ class FireAlarmController extends Controller
         $validator = Validator::make($request->all(), [
             'reference_no' => 'nullable|string|max:40|unique:fire_alarm_inspections,reference_no,' . $id,
             'inspection_date' => 'required|date',
-            'location_id' => 'nullable|exists:fire_alarm_locations,id_location',
+            'location_id' => 'nullable|exists:fire_alarm_location,id_location',
             'area_id' => 'nullable|exists:areas,id',
             'qr_code_id' => 'nullable|exists:asset_qr_codes,id',
             'inspector_id' => 'nullable|exists:users,id',
